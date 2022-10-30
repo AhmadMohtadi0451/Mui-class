b5c7d994e7ec168f87da7634f992e7453fc6c5e5
Ahmad Mohtadi <ahmadmohtadi0451@gmail.com>
2022-10-30 18:42:22 +0330

    customize theme added

---
+++ src/Themes/Themes.js
@@ -0,0 +1,79 @@
+import { createTheme } from "@mui/material/styles";
+
+const Themes = createTheme({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          background: "#EEE",
+          ".margin": {
+            margin: 50,
+          },
+          a: {},
+          "#myId": {},
+        },
+      },
+    },
+    MuiButton: {
+      variants: [
+        {
+          props: { variant: "mainButton" },
+          style: {
+            color: "#fff",
+            backgroundColor: "#50C2C9",
+            borderRadius: "20px",
+            textAlign: "center",
+            height: 60,
+            "&:hover": {
+              backgroundColor: "#50C2C9",
+            },
+          },
+        },
+        {
+          props: { variant: "secondaryButton" },
+          style: {},
+        },
+      ],
+    },
+  },
+  MuiCheckbox: {
+    variants: [
+      {
+        props: { variant: "mainCheckbox" },
+        style: {
+          color: "#94F6FC",
+        },
+      },
+    ],
+  },
+  palette: {
+    primary: {
+      main: "#50C2C9",
+      secondary: "#000000D9",
+      light: "#94F6FC",
+    },
+    background: "#EEE",
+  },
+  typography: {
+    fontFamily: "Poppins",
+    fontWeight: 400,
+    fontSize: "13px",
+    lineHeight: 19.5,
+    h1: {
+      fontSize: 18,
+      fontWeight: 700,
+      lineHeight: "27px",
+    },
+    h2: {
+      fontWeight: 600,
+      lineHeight: "21px",
+      fontSize: 14,
+    },
+  },
+  link: {
+    color: "#50C2C9",
+    underline: "none",
+  },
+});
+
+export default Themes;
